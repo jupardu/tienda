bc3bc23adc6a9f1ab0ef449d8de7578546810d91
Angular CLI <angular-cli@angular.io>
2017-10-11 18:43:15 -0500

    chore: initial commit from @angular/cli

---
+++ src/java/controlador/ProductoFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import entidades.Producto;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Juan
+ */
+@Stateless
+public class ProductoFacade extends AbstractFacade<Producto> {
+
+    @PersistenceContext(unitName = "tiendaPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public ProductoFacade() {
+        super(Producto.class);
+    }
+    
+}
