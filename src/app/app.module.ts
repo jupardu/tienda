9388f697016c5cda2e37132c35628fb35f4ae19f
Julian Castaño <jualca55@gmail.com>
2017-10-12 19:56:19 -0500

    se implementan los routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+import { IndexComponent } from './cliente/index/index.component'
+import { CrearProductoComponent } from './producto/crear-producto/crear-producto.component';
+
 import {ClienteModule} from './cliente/cliente.module';
 import {ProductoModule} from './producto/producto.module';
 import {VentaModule} from './venta/venta.module';
@@ -8,6 +12,10 @@ import {VentaModule} from './venta/venta.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 
+const appRoutes: Routes =[
+  { path: 'cliente/index', component: IndexComponent },
+  { path: 'producto/crear-cliente', component: CrearProductoComponent }
+]
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +25,10 @@ import { MenuComponent } from './menu/menu.component';
     BrowserModule,
     ClienteModule,
     ProductoModule,
-    VentaModule
+    VentaModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
